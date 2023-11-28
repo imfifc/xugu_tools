236f9aa211e27b431ae166075ec99e1db7cd05d6
Pjmzhu <1193690571@qq.com>
2023-11-28 18:16:45 +0800

    fix: make_data5.py

--- demo/make_data5.py
+++ demo/make_data5.py
@@ -7,75 +7,19 @@ import time
 import uuid
 from datetime import datetime, timedelta
 from multiprocessing import freeze_support
+# from random import seed
+
 from faker import Faker
 
 import xgcondb
 
-fake = Faker(locale='zh_CN')
+
 
 
 def get_cur(db_host, db_port, db_user, db_pwd, db_name):
     conn = xgcondb.connect(host=db_host, port=db_port, database=db_name, user=db_user, password=db_pwd, charset="utf8")
     cur = conn.cursor()
     return cur
-
-
-# 参数解析前置，多进程才能不报错
-def parse_args():
-    parser = argparse.ArgumentParser(
-        # description='这是一个数据库环境采集工具',
-        prefix_chars='-'
-    )
-    # 添加位置参数
-    # parser.add_argument('input_file', help='输入文件的路径')
-    # 添加可选参数
-    # parser.add_argument('-o', '--output', help='输出文件的路径')
-    parser.add_argument('-H', '--host', help='输入数据库ip地址')
-    parser.add_argument('-P', '--port', help='Port number 数据库端口', type=int, default=5138)
-    parser.add_argument('-u', '--user', help='输入数据库 用户')
-    parser.add_argument('-p', '--pwd', help='输入数据库密码')
-    parser.add_argument('-d', '--database_name', help='输入数据库名')
-    # 添加标志参数
-    parser.add_argument('-v', '--verbose', action='store_true', help='是否显示详细信息')
-    args = parser.parse_args()
-    # 访问解析后的参数
-    # input_file = args.input_file
-    # output_file = args.output
-    host = args.host
-    port = args.port
-    user = args.user
-    password = args.pwd
-    db = args.database_name
-    verbose = args.verbose
-
-    # 在这里可以根据解析的参数执行相应的操作
-    if len(sys.argv) == 1:
-        host = input("请输入ip: ")
-        port = input("请输入端口: ")
-        user = input("请输入用户: ")
-        password = input("请输入密码: ")
-        db = input("请输入数据库: ")
-    if verbose:
-        print("显示详细信息")
-    if not host:
-        parser.print_help()
-        raise Exception('没有输入ip !!!\n')
-    if not port:
-        parser.print_help()
-        raise Exception('没有输入port !!!\n')
-    if not user:
-        parser.print_help()
-        raise Exception('没有输入user !!!\n')
-    if not password:
-        parser.print_help()
-        raise Exception('没有输入password !!!\n')
-    if not db:
-        parser.print_help()
-        raise Exception('没有输入数据库 !!!\n')
-    # if host and port and user and password and db:
-    #     print(f'host: {host} port: {port} user: {user} password: {password} db: {db} \n')
-
-    return host, port, user, password, db
 
 
 # 存储过程中不能有注释
@@ -174,12 +118,14 @@ def generate_dates(n):
 
 
 def insert_many(date, nums, db_host, db_port, db_user, db_pwd, db_name):
-    chinese_text = fake.text(max_nb_chars=100)
     cur = get_cur(db_host, db_port, db_user, db_pwd, db_name)
     sql = "insert into product5 values(?,?,?,?,?,?,?)"
+    fake = Faker(locale='zh_CN')
+    Faker.seed()  # 使用不同的种子
     # print(uuid.uuid4(),type(uuid.uuid4()))
     rows = []
     for i in range(nums):
+        chinese_text = fake.text(max_nb_chars=100)
         data = (str(uuid.uuid4()), f'零食大礼包{i}', chinese_text, date, date, fake.city(), fake.city())
         rows.append(data)
     print(len(rows), rows[:1])
@@ -187,11 +133,11 @@ def insert_many(date, nums, db_host, db_port, db_user, db_pwd, db_name):
 
 
 # 多进程调用
-def multi_process(dates, rows, db_host, db_port, db_user, db_pwd, db_name):
+def multi_process(dates, nums, db_host, db_port, db_user, db_pwd, db_name):
     processes = []
     for date in dates:
         process = multiprocessing.Process(target=insert_many,
-                                          args=(date, rows, db_host, db_port, db_user, db_pwd, db_name))
+                                          args=(date, nums, db_host, db_port, db_user, db_pwd, db_name))
         processes.append(process)
     for process in processes:
         process.start()
@@ -201,26 +147,25 @@ def multi_process(dates, rows, db_host, db_port, db_user, db_pwd, db_name):
 
 
 def once_proc():
-    rows = int(input("请输入表行数: "))
+    nums = int(input("请输入表行数: "))
     days = int(input("请输入天数: "))
 
     dates = generate_dates(days)
     print(dates)
     start = time.time()
-    multi_process(dates, rows, db_host, db_port, db_user, db_pwd, db_name)
+    multi_process(dates, nums, db_host, db_port, db_user, db_pwd, db_name)
     end = time.time() - start
     show('product5')
-    print(f'耗时{end:.2f}秒', f'tps:{(rows * days / end):.2f} 行/s')
+    print(f'耗时{end:.2f}秒', f'tps:{(nums * days / end):.2f} 行/s')
 
 
 if __name__ == '__main__':
     freeze_support()
     db_host = '10.28.20.101'
-    db_port = 6325
+    db_port = 6326
     db_user = 'SYSDBA'
     db_pwd = 'SYSDBA'
     db_name = 'SYSTEM'
-    # db_host, db_port, db_user, db_pwd, db_name = parse_args()
     cur = get_cur(db_host, db_port, db_user, db_pwd, db_name)
     create_random_package()
     cur.execute('set max_loop_num to 0')

22a1a3402721e9317935fe9b32df1a0a13b0f209
Pjmzhu <1193690571@qq.com>
2023-11-29 10:13:21 +0800

    fix: fix

--- demo/redme.py
+++ demo/redme.py
@@ -1,6 +1,7 @@
 # pyinstaller -c -F  --clean  --hidden-import=xgcondb     .\connection_pool.py
 # connection_pool.exe -H  10.28.20.101 -P6326 -uSYSDBA -p SYSDBA -d SYSTEM
 
+# pyinstaller -c -F  --clean  --hidden-import=xgcondb   --hidden-import=faker make_data5.py
 # linux 需要,  cp libxugusql.so /usr/lib64/
 # 存储过程不要有注释,连接串编码必须为utf8
 #  加索引，清除表及数据,先清除事务,arm随机函数
